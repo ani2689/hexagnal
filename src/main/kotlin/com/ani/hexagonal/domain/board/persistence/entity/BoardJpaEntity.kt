b7bf6fd1adcf39daf72b37846e09d12248374bd1
ani2689 <ojno7777@gmail.com>
2023-09-17 23:46:21 +0900

    :recycle: 테이블 이름 수정 및 컬럼 추가

--- src/main/kotlin/com/ani/hexagonal/domain/board/persistence/entity/BoardJpaEntity.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/board/persistence/entity/BoardJpaEntity.kt
@@ -1,21 +1,33 @@
 package com.ani.hexagonal.domain.board.persistence.entity
 
+import com.ani.hexagonal.domain.user.persistence.entity.User
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
 import org.springframework.data.annotation.Id
 
 @Entity
-data class BoardJpaEntity(
+@Table(name = "board")
+class BoardJpaEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     val id: Long,
+
     val title: String,
+
     val content: String,
+
     @ManyToMany
     @JoinColumn(name = "categories_id")
-    val categories: MutableList<CategoryJpaEntity>
+    val categories: MutableList<CategoryJpaEntity>,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val writer: User
 )
--- src/main/kotlin/com/ani/hexagonal/domain/board/persistence/entity/CategoryJpaEntity.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/board/persistence/entity/CategoryJpaEntity.kt
@@ -4,12 +4,15 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
+import jakarta.persistence.Table
 import org.springframework.data.annotation.Id
 
 @Entity
-data class CategoryJpaEntity(
+@Table(name = "category")
+class CategoryJpaEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     val id: Long,
+
     val name: String
 )
