a216f6fac83dabd1bee912fbb6b080ce05691716
ani2689 <ojno7777@gmail.com>
2023-09-17 23:31:29 +0900

    :heavy_plus_sign: 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
