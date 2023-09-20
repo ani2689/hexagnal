80e31f784df83e8fb2ecb8c198bcc52811ef8686
ani2689 <ojno7777@gmail.com>
2023-09-21 08:03:26 +0900

    :recycle: Long -> uuid

--- src/main/kotlin/com/ani/hexagonal/domain/board/application/service/QueryBoardService.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/board/application/service/QueryBoardService.kt
@@ -2,10 +2,11 @@ package com.ani.hexagonal.domain.board.application.service
 
 import com.ani.hexagonal.domain.board.application.model.Board
 import com.ani.hexagonal.domain.board.application.model.Category
+import java.util.UUID
 
 interface QueryBoardService {
     fun findAllBoard(): List<Board>
     fun findBoardById(id: Long): Board
-    fun findBoardByUUID(uuid: Long): List<Board>
+    fun findBoardByUUID(uuid: UUID): List<Board>
     fun findBoardByCategory(category: Category): List<Board>
 }
