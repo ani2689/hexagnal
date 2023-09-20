5b5d9e0f7b38f8a0e655e9aadb6d7e6aa3af0680
ani2689 <ojno7777@gmail.com>
2023-09-20 19:53:47 +0900

    :recycle: user 도메인객체 -> UUID

--- src/main/kotlin/com/ani/hexagonal/domain/board/application/facade/BoardFacade.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/board/application/facade/BoardFacade.kt
@@ -5,15 +5,15 @@ import com.ani.hexagonal.domain.board.presentation.data.req.CreateBoardRequestDa
 import com.ani.hexagonal.domain.board.presentation.data.req.ModifyBoardRequestData
 import com.ani.hexagonal.domain.board.presentation.data.res.BoardDetailResponseData
 import com.ani.hexagonal.domain.board.presentation.data.res.BoardResponseData
-import com.ani.hexagonal.domain.user.application.model.User
+import java.util.UUID
 
 interface BoardFacade {
     fun createBoard(createBoardRequestData: CreateBoardRequestData)
     fun queryAllBoard() : List<BoardResponseData>
     fun queryBoardByCategory(categories: List<CategoryRequestData>) : List<BoardResponseData>
     fun queryBoardById(id: Long) : BoardDetailResponseData
-    fun queryBoardByUser(user: User) : List<BoardResponseData>
+    fun queryBoardByUUID(uuid: UUID) : List<BoardResponseData>
     fun checkBoard(modifyBoardRequestData: ModifyBoardRequestData)
     fun deleteBoardById(id: Long)
-    fun deleteAllBoardByUser(user: User)
+    fun deleteAllBoardByUser(uuid: UUID)
 }
