8459c723908dc43bacc03dccb9da48f0fa92218f
ani2689 <ojno7777@gmail.com>
2023-09-21 10:25:50 +0900

    :sparkles: 웹 어댑터 정의

--- src/main/kotlin/com/ani/hexagonal/domain/board/persistence/BoardPersistenceAdapter.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/board/persistence/BoardPersistenceAdapter.kt
@@ -1,10 +1,13 @@
 package com.ani.hexagonal.domain.board.persistence
 
 import com.ani.hexagonal.domain.board.application.model.Board
+import com.ani.hexagonal.domain.board.application.model.Category
 import com.ani.hexagonal.domain.board.application.spi.BoardPort
 import com.ani.hexagonal.domain.board.persistence.mapper.toDomain
 import com.ani.hexagonal.domain.board.persistence.mapper.toEntity
 import com.ani.hexagonal.domain.board.persistence.repository.BoardRepository
+import com.ani.hexagonal.domain.user.application.model.User
+import com.ani.hexagonal.domain.user.persistence.mapper.toEntity
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,4 +19,24 @@ class BoardPersistenceAdapter(
 
     override fun delete(board: Board) =
         boardRepository.delete(board.toEntity())
+
+    override fun findAllBoard() =
+        boardRepository.findAll()
+            .map {
+                it.toDomain()
+            }
+
+    override fun findBoardById(id: Long) =
+        boardRepository.findById(id).get().toDomain()
+
+    override fun findBoardByWriter(writer: User): List<Board> =
+        boardRepository.findByWriter(writer.toEntity())
+            .map {
+                it.toDomain()
+            }
+    override fun findBoardByCategory(category: Category): List<Board> =
+        boardRepository.findByCategoryContaining(category.toEntity())
+            .map {
+                it.toDomain()
+            }
 }
