6bf8f73b35be97639229d60c7aed29642f5d75af
ani2689 <ojno7777@gmail.com>
2023-09-25 08:03:48 +0900

    :bug: @GetMapping 안붙였어요

--- src/main/kotlin/com/ani/hexagonal/domain/auth/presentation/AuthWebAdapter.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/auth/presentation/AuthWebAdapter.kt
@@ -3,6 +3,7 @@ package com.ani.hexagonal.domain.auth.presentation
 import com.ani.hexagonal.domain.auth.application.facade.AuthFacade
 import com.ani.hexagonal.domain.auth.presentation.data.res.GoogleLoginUrlResponseData
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 class AuthWebAdapter (
     private val authFacade: AuthFacade
 ){
+    @GetMapping
     fun queryGoogleLoginUrl(): ResponseEntity<GoogleLoginUrlResponseData> =
         authFacade.queryGoogleLoginUrl()
             .let { ResponseEntity.ok(it) }
