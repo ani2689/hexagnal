15f094f5c846aa14d01b78ac23fef3ac8aa444ae
ani2689 <ojno7777@gmail.com>
2023-09-24 23:14:52 +0900

    :sparkles: @DomainService 어노테이션 붙이기

--- src/main/kotlin/com/ani/hexagonal/domain/user/application/service/UserService.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/user/application/service/UserService.kt
@@ -1,5 +1,8 @@
 package com.ani.hexagonal.domain.user.application.service
 
+import com.ani.hexagonal.global.annotation.service.DomainService
+
+@DomainService
 class UserService (
     queryUserService: QueryUserService
 ) : QueryUserService by queryUserService
