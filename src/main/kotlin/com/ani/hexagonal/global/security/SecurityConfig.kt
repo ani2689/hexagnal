41206f716c72d10d117e75e8c46a9ef9eaa17b90
ani2689 <ojno7777@gmail.com>
2023-09-25 19:54:36 +0900

    :sparkles: 시큐리티 config 작성

---
+++ src/main/kotlin/com/ani/hexagonal/global/security/SecurityConfig.kt
@@ -0,0 +1,44 @@
+package com.ani.hexagonal.global.security
+import com.ani.hexagonal.global.config.FilterConfig
+import com.ani.hexagonal.global.security.spi.JwtParserPort
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+class SecurityConfig (
+    private val jwtParserPort: JwtParserPort
+){
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain  = http
+            .cors { it.disable() }
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .authorizeHttpRequests{
+                it
+                    .requestMatchers(HttpMethod.GET,"/auth").permitAll()
+                    .requestMatchers(HttpMethod.GET,"/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.DELETE,"/auth/logout").authenticated()
+
+                    .requestMatchers(HttpMethod.POST, "/board").authenticated()
+                    .requestMatchers(HttpMethod.GET, "/board").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/board/category").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/board/{uuid}").authenticated()
+                    .requestMatchers(HttpMethod.GET, "/board/{uuid}").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/board/{board_id}").permitAll()
+                    .requestMatchers(HttpMethod.PATCH, "/board/{board_id}").authenticated()
+                    .requestMatchers(HttpMethod.DELETE, "/board/{board_id}").authenticated()
+
+                    .anyRequest().authenticated()
+            }
+            .apply(FilterConfig(jwtParserPort)).and()
+            .build()
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
+}
