e829f40bbdcb01eee89d0e0975759789eafc7f80
ani2689 <ojno7777@gmail.com>
2023-10-03 23:55:03 +0900

    :sparkles: 리프레쉬 토큰 entity 생성

---
+++ src/main/kotlin/com/ani/hexagonal/domain/auth/persistence/entity/RefreshTokenEntity.kt
@@ -0,0 +1,21 @@
+package com.ani.hexagonal.domain.auth.persistence.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+
+@RedisHash("refresh_token")
+data class RefreshTokenEntity(
+
+    @Id
+    val refreshToken: String,
+
+    val userId: UUID,
+
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    val expiredAt: Int
+
+)
