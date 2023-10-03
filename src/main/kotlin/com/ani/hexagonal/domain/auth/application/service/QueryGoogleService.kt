906872e1a17ebce565084175e80d10f313cbd34a
ani2689 <ojno7777@gmail.com>
2023-10-03 21:53:03 +0900

    :sparkles: 토큰으로 유저 정보 받아오기 기능 추상화

--- src/main/kotlin/com/ani/hexagonal/domain/auth/application/service/QueryGoogleService.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/auth/application/service/QueryGoogleService.kt
@@ -2,8 +2,10 @@ package com.ani.hexagonal.domain.auth.application.service
 
 import com.ani.hexagonal.domain.auth.application.model.GoogleLoginUrl
 import com.ani.hexagonal.domain.auth.application.model.GoogleToken
+import com.ani.hexagonal.domain.auth.application.model.GoogleUser
 
 interface QueryGoogleService {
     fun queryGoogleLoginUrl(): GoogleLoginUrl
     fun queryGoogleToken(code: String): GoogleToken
+    fun queryGoogleUserByGoogleToken(googleToken: GoogleToken): GoogleUser
 }
