f126f82f10f98c0460b1243f85c72a927570094d
ani2689 <ojno7777@gmail.com>
2023-10-03 23:29:08 +0900

    :sparkles: 유저 도메인에 룰 추가

--- src/main/kotlin/com/ani/hexagonal/domain/user/application/model/User.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/user/application/model/User.kt
@@ -1,9 +1,11 @@
 package com.ani.hexagonal.domain.user.application.model
 
+import com.ani.hexagonal.domain.user.application.model.role.UserRole
 import java.util.UUID
 
 data class User(
     val id: UUID,
     val name: String,
-    val email: String
+    val email: String,
+    val role: List<UserRole>
 )
--- src/main/kotlin/com/ani/hexagonal/domain/user/persistence/mapper/UserMapper.kt
+++ src/main/kotlin/com/ani/hexagonal/domain/user/persistence/mapper/UserMapper.kt
@@ -6,11 +6,13 @@ import com.ani.hexagonal.domain.user.persistence.entity.UserJpaEntity
 fun User.toEntity() = UserJpaEntity(
     this.id,
     this.name,
-    this.email
+    this.email,
+    this.role.toMutableList()
 )
 
 fun UserJpaEntity.toDomain() = User(
     this.id,
     this.name,
-    this.email
+    this.email,
+    this.role
 )
